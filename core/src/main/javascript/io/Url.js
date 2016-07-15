595146602ce7de0531104d1f93b23a67d7938abb
Wesley Miaw <wmiaw@netflix.com>
2016-07-15 14:27:25 -0700

    Fix bug that would attempt to mark the backing ByteArrayInputStream buffer only if undefined.

--- core/src/main/javascript/io/Url.js
+++ core/src/main/javascript/io/Url.js
@@ -221,7 +221,7 @@ var IHttpLocation = util.Class.create({
 
         /** @inheritDoc */
         mark: function mark() {
-            if (!this._buffer)
+            if (this._buffer)
                 this._buffer.mark();
         },
 
