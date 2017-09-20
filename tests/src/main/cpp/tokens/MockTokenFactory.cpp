e326c353711aa7c02487f283a76cc343325948c0
Wesley Miaw <wmiaw@netflix.com>
2017-09-20 13:52:06 -0700

    Update 2017 copyright for modified files.

--- tests/src/main/cpp/tokens/MockTokenFactory.cpp
+++ tests/src/main/cpp/tokens/MockTokenFactory.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016 Netflix, Inc.  All rights reserved.
+ * Copyright (c) 2016-2017 Netflix, Inc.  All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 /**
- * Copyright (c) 2016 Netflix, Inc.  All rights reserved.
+ * Copyright (c) 2016-2017 Netflix, Inc.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
