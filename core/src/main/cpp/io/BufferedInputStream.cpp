3b5d05f27cbc214127857b498577e3eced083987
Alan Liddeke <aliddeke@netflix.com>
2018-09-06 09:33:12 -0700

    Change readlimit_ to int type, use checked math for safe assignment, remove flag to ignore signed comparison and cast where appropriate.

--- core/src/main/cpp/io/BufferedInputStream.cpp
+++ core/src/main/cpp/io/BufferedInputStream.cpp
@@ -16,10 +16,12 @@
 #include <io/BufferedInputStream.h>
 #include <io/ByteArrayOutputStream.h>
 #include <IOException.h>
-
+#include <numerics/safe_math.h>
 #include <algorithm>
+#include <limits>
 
 using namespace std;
+using base::internal::CheckedNumeric;
 
 namespace netflix {
 namespace msl {
@@ -47,12 +49,12 @@ void BufferedInputStream::mark(size_t readlimit)
     // Otherwise the existing buffer contains the correct data.
     //
     // Regardless set the new read limit.
-    readlimit_ = readlimit;
+    readlimit_ = CheckedNumeric<int>(readlimit).ValueOrDefault(-1);
 }
 
 void BufferedInputStream::reset()
 {
-    if (readlimit_ == (size_t)-1)
+    if (readlimit_ == -1)
         throw IOException("Cannot reset before input stream has been marked or if mark has been invalidated.");
 
     // Start reading from the beginning of the buffer.
@@ -79,7 +81,7 @@ int BufferedInputStream::read(ByteArray& out, size_t offset, size_t len, int tim
         // If the data is of sufficient size, return it.
         if (bufferedData.size() >= len) {
             copy(bufferedData.begin(), bufferedData.end(), out.begin() + offset);
-            return bufferedData.size();
+            return static_cast<int>(bufferedData.size());
         }
     }
 
@@ -94,7 +96,7 @@ int BufferedInputStream::read(ByteArray& out, size_t offset, size_t len, int tim
     // On end of stream, return the buffered data.
     if (count == -1) {
         copy(bufferedData.begin(), bufferedData.end(), out.begin() + offset);
-        return bufferedData.size();
+        return static_cast<int>(bufferedData.size());
     }
 
     // Append to the buffer.
@@ -105,13 +107,13 @@ int BufferedInputStream::read(ByteArray& out, size_t offset, size_t len, int tim
     bufpos_ += readCount;
 
     // Invalidate the mark if the requested data size exceeds the read limit.
-    if (bufpos_ > readlimit_)
+    if (bufpos_ > static_cast<size_t>(readlimit_))
         readlimit_ = -1;
 
     // Return the buffered data and the read data.
     copy(bufferedData.begin(), bufferedData.end(), out.begin() + offset);
     copy(sourceData.begin(), sourceData.begin() + readCount, out.begin() + offset + bufferedData.size());
-    return bufferedData.size() + readCount;
+    return static_cast<int>(bufferedData.size() + readCount);
 }
 
 int BufferedInputStream::skip(size_t n, int timeout)
@@ -127,14 +129,14 @@ int BufferedInputStream::skip(size_t n, int timeout)
 
         // If we skipped as much as requested return immediately.
         if (skipcount == n)
-            return skipcount;
+            return static_cast<int>(skipcount);
     }
 
     // We were not able to skip enough using just buffered data.
     ByteArray data(n - skipcount);
     int readcount = read(data, timeout);
-    if (readcount == -1) return skipcount;
-    return readcount + skipcount;
+    if (readcount == -1) return static_cast<int>(skipcount);
+    return static_cast<int>(readcount + skipcount);
 }
 
 }}} // namespace netflix::msl::io
