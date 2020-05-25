b2bec717f1acd67358addfac30e969b82f65c133
Wesley Miaw <wmiaw@netflix.com>
2020-05-25 14:16:55 -0700

    Add support for OpenSSL v1.1. For whatever reason DH_generate_key() creates a zero-length non-null public key BIGNUM, which prevents things from working properly.

--- core/src/main/cpp/crypto/opensslImpl/diffieHellman.cpp
+++ core/src/main/cpp/crypto/opensslImpl/diffieHellman.cpp
@@ -52,24 +52,47 @@ void dhGenKeyPair(const ByteArray& p, const ByteArray& g, ByteArray& pubKey, Byt
     BigNum gbn(BN_bin2bn(&g[0], CheckedNumeric<int>::cast(gSize).ValueOrDie(), NULL));
     if (!gbn.get())
         throw MslInternalException("dhGenKeyPair: BN_bin2bn() error making bignum g");
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     if (!(dh.get()->p = BN_dup(pbn.get())))
         throw MslInternalException("dhGenKeyPair: BN_dup() unable to duplicate DH prime");
     if (!(dh.get()->g = BN_dup(gbn.get())))
         throw MslInternalException("dhGenKeyPair: BN_dup() unable to duplicate DH generator");
+#else
+    BIGNUM * dupP = BN_dup(pbn.get());
+    if (!dupP)
+        throw MslInternalException("dhGenKeyPair: BN_dup() unable to duplicate DH prime");
+    BIGNUM * dupG = BN_dup(pbn.get());
+    if (!dupG)
+        throw MslInternalException("dhGenKeyPair: BN_dup() unable to duplicate DH generator");
+    if (!DH_set0_pqg(dh.get(), dupP, NULL, dupG))
+        throw MslInternalException("dhGenKeyPair: unable to set DH prime and generator");
+#endif
 
     // Generate the public/private key pair
     if (!DH_generate_key(dh.get()))
         throw MslInternalException("dhGenKeyPair: DH_generate_key() unable to generate key pair");
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     const BIGNUM * const pubBn = dh.get()->pub_key;
+#else
+    const BIGNUM * const pubBn = DH_get0_pub_key(dh.get());
+#endif
     const int pubSizeBytes = BN_num_bytes(pubBn);
+    if (pubSizeBytes == 0)
+        throw MslInternalException("dhGenKeyPair: DH_generate_key() generated zero-length public key");
     ByteArray pubBuf(static_cast<const size_t>(pubSizeBytes), 0);
     int sizeResult = BN_bn2bin(pubBn, &pubBuf[0]); (void) sizeResult;
     if (sizeResult != pubSizeBytes)
         throw MslInternalException("dhGenKeyPair: BN_bn2bin() error converting public key");
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     const BIGNUM * const privBn = dh.get()->priv_key;
+#else
+    const BIGNUM * const privBn = DH_get0_priv_key(dh.get());
+#endif
     const int privSizeBytes = BN_num_bytes(privBn);
+    if (privSizeBytes == 0)
+        throw MslInternalException("dhGenKeyPair: DH_generate_key() generated zero-length private key");
     ByteArray privBuf(static_cast<const size_t>(privSizeBytes), 0);
     sizeResult = BN_bn2bin(privBn, &privBuf[0]); (void) sizeResult;
     if (sizeResult != privSizeBytes)
@@ -98,14 +121,34 @@ void dhComputeSharedSecret(const ByteArray& remotePublicKey, const ByteArray& p,
     BigNum pbn(BN_bin2bn(&p[0], CheckedNumeric<int>::cast(pSize).ValueOrDie(), NULL));
     if (!pbn.get())
         throw MslInternalException("computeSharedSecret: BN_bin2bn() error making bignum from p");
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     if (!(dh.get()->p = BN_dup(pbn.get())))
         throw MslInternalException("computeSharedSecret: BN_dup() unable to duplicate DH prime");
+#else
+    BIGNUM * dupN = BN_dup(pbn.get());
+    if (!dupN)
+        throw MslInternalException("computeSharedSecret: BN_dup() unable to duplicate DH prime");
+    const BIGNUM * g = DH_get0_g(dh.get());
+    BIGNUM * dupG = (g != NULL) ? BN_dup(g) : NULL;
+    if (!dupG)
+        throw MslInternalException("computeSharedSecret: BN_dup() unable to duplicate DH generator");
+    if (!DH_set0_pqg(dh.get(), dupN, NULL, dupG))
+        throw MslInternalException("dhGenKeyPair: unable to set DH prime and generator");
+#endif
     CheckedNumeric<size_t> localPrivateKeySize(localPrivateKey.size());
     BigNum localPrivateKeyBn(BN_bin2bn(&localPrivateKey[0], CheckedNumeric<int>::cast(localPrivateKeySize).ValueOrDie(), NULL));
     if (!localPrivateKeyBn.get())
         throw MslInternalException("computeSharedSecret: BN_bin2bn() error making bignum from private key");
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     if (!(dh.get()->priv_key = BN_dup(localPrivateKeyBn.get())))
         throw MslInternalException("computeSharedSecret: BN_dup() unable to duplicate DH private key");
+#else
+    BIGNUM * dupPrivKey = BN_dup(localPrivateKeyBn.get());
+    if (!dupPrivKey)
+        throw MslInternalException("computeSharedSecret: BN_dup() unable to duplicate DH private key");
+    if (!DH_set0_key(dh.get(), NULL, dupPrivKey))
+        throw MslInternalException("computeSharedSecret: unable to set DH private key");
+#endif
 
     // make remotePublicKey into a BIGNUM
     CheckedNumeric<size_t> remotePublicKeySize(remotePublicKey.size());
